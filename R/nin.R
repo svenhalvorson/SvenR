85d6d9522b74f9830dd9f40fedaaf81043cdca36
Sven Halvorson <svenedmail@gmail.com>
2019-04-30 12:22:17 -0400

    cleaned documentation for nin and namespace

--- R/nin.R
+++ R/nin.R
@@ -1,11 +1,13 @@
-# Not In
-#' @description clearer, for me at least, version of !x %in% y
-#' @author Sven Halvorson
+#' @title Not In
+#' @description Inverse of infix \code{\%in\%}
+#' @author Somebody else, not sure who
 #' @examples 'a' %nin% letters
 #' 'a' %nin% LETTERS
 #' @param x values to be matched
 #' @param y values to be matched against
 #' @seealso match
-`%nin%` = function(x,y){
+#' @rdname nin
+#' @export
+"%nin%" = function(x,y){
 !(x %in% y)
 }
