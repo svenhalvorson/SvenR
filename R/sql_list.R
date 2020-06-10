67abcde86bc5bb22d64e4f5dbb65d3fbb6222d53
svenhalvorson <svenedmail@gmail.com>
2020-06-10 15:20:08 -0400

    added wildcard parameter to sql_list

--- R/sql_list.R
+++ R/sql_list.R
@@ -2,18 +2,23 @@
 #' @description Format an atomic vector for use with a SQL 'IN' statements
 #' @param x Atomic character or numeric vector
 #' @param quote Should each entry be surrounded with single quotes?
+#' @param wildcard Should each entry be surrounded with percent signs? Used for wildcard matching
 #' @author Sven Halvorson
 #' @examples
 #' paste(
 #'   'SELECT nonsense',
 #'   'FROM your_table',
-#'   'WERE nonsense IN',
-#'   sql_list(letters[1:5]),
+#'   'WERE nonsense LIKE ANY',
+#'   sql_list(letters[1:5], wildcard = TRUE),
 #'   ';'
 #' )
 #' @export
 #'
-sql_list = function(x, quote = TRUE){
+sql_list = function(
+  x,
+  quote = TRUE,
+  wildcard = FALSE
+){
 
   if(
     any(
@@ -24,7 +29,22 @@ sql_list = function(x, quote = TRUE){
     stop('x must be an atomic character or numeric vector')
   }
 
-  if(quote){
+  if(!quote & wildcard){
+    stop('quote must be TRUE to use wildcard')
+  }
+
+  if(wildcard){
+    paste0(
+      "('%",
+      paste0(
+        x,
+        collapse = "%', '%"
+      ),
+      "%')"
+    )
+
+  }
+  else if(quote){
     paste0(
       "('",
       paste0(
